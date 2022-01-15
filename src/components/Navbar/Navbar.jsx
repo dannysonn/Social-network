02dc0e7385e09e1f056370c39222966c7e03b2f2
Danya <dannyson5@bk.ru>
2022-01-15 19:58:33 +0300

    add NavLink instead of <a> for SPA

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,23 @@
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
+
     return (<nav className={classes.nav}>
         <div className={classes.item}>
-            <a href="/profile">Profile</a>
+            <NavLink to='/profile' className={({ isActive })=> isActive ? classes.active : ""}>Profile</NavLink>
         </div>
-        <div className={`${classes.item} ${classes.active}`}>
-            <a href="/dialogs">Messages</a>
-        </div>
-        <div className={classes.item}>
-            <a href='/news'>News</a>
+        <div className={`${classes.item}`}>
+            <NavLink to="/dialogs" className={({ isActive })=> isActive ? classes.active : ""}>Messages</NavLink>
         </div>
         <div className={classes.item}>
-            <a href='/music'>Music</a>
+            <NavLink to='/news' className={({ isActive })=> isActive ? classes.active : ""}>News</NavLink>
+        </div>
+        <div className={classes.item} >
+            <NavLink to='/music' className={({ isActive })=> isActive ? classes.active : ""}>Music</NavLink>
         </div>
         <div className={classes.item}>
-            <a href='/settings'>Settings</a>
+            <NavLink to='/settings' className={({ isActive })=> isActive ? classes.active : ""}>Settings</NavLink>
         </div>
     </nav>);
 }
